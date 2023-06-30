7a6664d89b016a9fc2d52070ddae0cc59b21bae5
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-06-30 18:47:15 -0300

    ref: typing id generic joi schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,5 +1,5 @@
 import Joi from "joi";
 
-const genericSchemas = { id: Joi.object({ id: Joi.number().min(1).required() }) };
+const genericSchemas = { id: Joi.object<{ id: number }>({ id: Joi.number().min(1).required() }) };
 
 export default genericSchemas;
